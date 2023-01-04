d01d6b4245b6fe8379bc1064936ac4a4e81da47e
Shea Murphy <shea@rocketmakers.com>
2023-01-04 22:38:23 +0000

    chore(shared): remove first and last name from payload schemas

--- projectImpact/layouts/forgotPassword/model.ts
+++ projectImpact/layouts/forgotPassword/model.ts
@@ -6,20 +6,14 @@
 export interface IModel {
   username: string;
   returnUrl: string;
-  firstName: string;
-  lastName: string;
 }
 
 export const sampleData: IModel[] = [
   {
-    firstName: 'Joe',
-    lastName: 'Bloggs',
     username: 'test@rocketmakers.com',
     returnUrl: 'https://www.rocketmakers.com',
   },
   {
-    firstName: 'Jim',
-    lastName: 'Bloggs',
     username: 'test2@rocketmakers.com',
     returnUrl: 'https://www.rocketmakers.com',
   },
